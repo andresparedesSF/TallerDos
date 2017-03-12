f8970ae6877d77f004be3b3d6f4a3c4f165464ef
Andrés Paredes <Andrs Paredes@DESKTOP-D2CALG9>
2017-03-11 19:17:37 -0500

    Segundo commit

--- ParedesAndres_TallerDos/src/MainApp.java
+++ ParedesAndres_TallerDos/src/MainApp.java
@@ -1,4 +1,31 @@
+import processing.core.PApplet;
 
-public class MainApp {
-
+public class MainApp extends PApplet{
+	
+	public static void main(String[] args) {
+		PApplet.main("MainApp");
+	}
+	
+	Logica app;
+	
+	@Override
+	public void settings() {
+		size(1200,700);
+	}
+	
+	@Override
+	public void setup() {
+		app = new Logica(this);
+	}
+	
+	@Override
+	public void draw() {
+		background(0);
+	}
+	
+	@Override
+	public void keyPressed() {
+		
+	}
+	
 }
