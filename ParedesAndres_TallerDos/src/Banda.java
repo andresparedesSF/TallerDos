d70da821b244e752daf11e1cdf2b78292b08c8a9
andresparedeSF <andresparedes0326@gmail.com>
2017-03-11 19:42:53 -0500

    Agregar imagenes

---
+++ ParedesAndres_TallerDos/src/Banda.java
@@ -0,0 +1,16 @@
+import processing.core.PApplet;
+
+public class Banda {
+
+	private PApplet app;
+	private int posX;
+	private int posY;
+	
+	public Banda(PApplet app, int posX, int posY){
+		
+		this.app = app;
+		this.posX = posX;
+		this.posY = posY;
+		
+	}
+}
